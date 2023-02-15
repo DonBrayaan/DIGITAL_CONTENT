5d2ac4b037923e90a02f916045d79f33d93dc2b2
ZairaPardo <117328994+ZairaPardo@users.noreply.github.com>
2023-02-14 20:31:31 -0500

    IDevolucion - facade

--- src/main/java/com/DigitalContentV2/DigitalContentv2/facade/IDevolucion.java
+++ src/main/java/com/DigitalContentV2/DigitalContentv2/facade/IDevolucion.java
@@ -7,8 +7,11 @@ import com.DigitalContentV2.DigitalContentv2.modelo.Devolucion;
 public interface IDevolucion {
 
 	public List<Devolucion> encontrarTodo();
+	public List<Devolucion> encontrarporEstado();
+	public Devolucion encontrarId(Integer idDevolucion);
 	public void crear(Devolucion devolucion);
 	public void actualizar(Devolucion devolucion);
-	public void eliminar(Devolucion devolucion);
+	public void eliminar(Integer idDevolucion);
+	public void actualizarEstado(Devolucion devolucion);
 	
 }
