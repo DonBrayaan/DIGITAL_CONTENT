ac936fb02c5b955f644cc7fb9db7a78556e315ac
Brayan Salguero <107779782+DonBrayaan@users.noreply.github.com>
2023-02-16 10:23:58 -0500

    Add files via upload

--- src/main/resources/static/barrioJS/barrio.js
+++ src/main/resources/static/barrioJS/barrio.js
@@ -1,16 +1,32 @@
-$(document).ready(function () {
-    $('#exampleS').DataTable();
+$(document).ready(function() {
+	$('#exampleS').DataTable({
+		dom: 'Bfrtilp',
+		buttons: [
+			{
+				extend: 'excelHtml5',
+				text: '<i class="fa-solid fa-file-excel"></i>',
+				titleAttr: 'Exportar a Excel',
+				className: 'btn btn-success'
+			},
+			{
+				extend: 'pdfHtml5',
+				text: '<i class="fa-solid fa-file-pdf"></i>',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			}
+		]
+	});
 });
 
 
-function eliminar(){
+function eliminar() {
 
 	var idBarrio = document.getElementById("txt_id").value;
-	location.href="/admin/barrio/eliminare/" + idBarrio;
-	
+	location.href = "/admin/barrio/eliminare/" + idBarrio;
+
 }
 
-function confirmar(idBarrio){
-	document.getElementById("txt_id").value=idBarrio;
+function confirmar(idBarrio) {
+	document.getElementById("txt_id").value = idBarrio;
 
 }
