63d95851aa5cb9eff7be07eb8a3a1681b0bfd261
ZairaPardo <117328994+ZairaPardo@users.noreply.github.com>
2023-02-20 17:07:25 -0500

    Reportes

--- src/main/resources/static/generoJS/genero.js
+++ src/main/resources/static/generoJS/genero.js
@@ -1,5 +1,21 @@
 $(document).ready(function () {
-    $('#exampleS').DataTable();
+    $('#exampleS').DataTable({
+		dom: 'Bfrtilp',
+		buttons: [
+			{
+				extend: 'excelHtml5',
+				text: '<i class="fa-solid fa-file-excel"></i>',
+				titleAttr: 'Exportar a Excel',
+				className: 'btn btn-success'
+			},
+			{
+				extend: 'pdfHtml5',
+				text: '<i class="fa-solid fa-file-pdf"></i>',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			}
+		]
+	});
 });
 
 
